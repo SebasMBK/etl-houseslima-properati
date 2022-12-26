a4d37d4ca78b360bf1189531724ffb699e5585e3
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@kyndryl.com>
2022-12-26 13:27:59 -0500

    changes on the comments

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,7 +2,9 @@
 #This file is only for running inside the docker container.
 #This will ask for you AWS keys
 aws configure
-#The next 3 lines of code starts terraform and creates the Infra
+#The next 2 lines of code starts terraform and creates the Infra
 terraform -chdir=/home/project/terraform init
 terraform -chdir=/home/project/terraform apply
+#This will give the right permissions to the ec2key for the ec2instance where our web application 
+#will be hosted
 chmod 400 /home/project/ansible/ec2key
