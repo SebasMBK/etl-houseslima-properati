fc881ce6b3bfa3ee8af5e43ffd0767f75090ef7b
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@kyndryl.com>
2023-01-23 09:50:38 -0500

    deleted run_pipeline

--- run_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#This file is only for running inside the docker container.
-#This will ask for you AWS keys
-aws configure
-#The next 2 lines of code starts terraform and creates the Infra
-terraform -chdir=/home/project/terraform init
-terraform -chdir=/home/project/terraform apply
